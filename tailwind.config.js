1a05ea7a244023a8612fa1bc20e7763eebc3f32c
Thefriendlykoala <91312002+Thefriendlykoala@users.noreply.github.com>
2025-01-18 01:13:58 +1100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "theme": {
+    "container": {
+      "center": true,
+      "padding": "2rem",
+      "screens": {
+        "2xl": "1400px"
+      }
+    }
+  },
+  "content": [
+    "./src/**/*.{ts,tsx}"
+  ],
+  "darkMode": [
+    "class"
+  ]
+}
